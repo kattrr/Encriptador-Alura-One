eb8d2ac8896bd3d540a259057c9910a94b1c15eb
Kathering <katheriverar@gmail.com>
2023-05-30 15:22:04 -0500

    Encriptador de texto

---
+++ JS/desencriptar.js
@@ -0,0 +1,26 @@
+function desencriptar() {
+    let texto = document.getElementById('textArea').value;
+    let tituloMensaje = document.getElementById('tituloSms');
+    let parrafo = document.getElementById('parrafo');
+    let img = document.getElementById('img1');
+  
+    let textoCifrado = texto
+      .replaceAll('enter', 'e')
+      .replaceAll('imes', 'i')
+      .replace('ai', 'a')
+      .replace('ober', 'o')
+      .replace('ufat', 'u');
+    
+      if (texto.length != 0) {
+        document.getElementById('textArea').value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        imgo.src = "../Img/img2.png"
+      } else {
+        img.src = "../Img/img3.png";
+        alert("Ingrese un texto primero");
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        
+    }
+}
---
+++ JS/encriptar.js
@@ -0,0 +1,24 @@
+function encriptar() {
+    let texto = document.getElementById('textArea').value;
+    let tituloMensaje = document.getElementById('tituloSms');
+    let parrafo = document.getElementById('parrafo');
+    let img = document.getElementById('img1');
+
+    let textoEncriptado = texto
+        .replaceAll('e', 'enter')
+        .replaceAll('i', 'imes')
+        .replaceAll('a', 'ai')
+        .replaceAll('o', 'ober')
+        .replaceAll('u', 'ufat');
+    if (texto.length != 0) {
+        document.getElementById('textArea').value = textoEncriptado;
+        tituloMensaje.textContent = "Texto Encriptado Exitosamente!";
+        parrafo.textContent = "";
+        img.src = "../Img/img2.png"
+    }else{
+        alert("Ingrese un texto primero");
+        img.src = "../Img/img3.png"
+        tituloMensaje.textContent = "No se ha encontrado ningun mensaje";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
+    }
+}
